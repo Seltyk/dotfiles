72c65c025136122f30c9edbde5d49e5efa643035
wundrweapon <whhacker.dcx@gmail.com>
2020-06-14 21:36:35 -0400

    Added a few more zsh aliases - mv, cp: confirm before overwrite - ffmpeg: shortcut for recording to FLAC (not working?) - gpg2: assign GPG_TTY so GPG works

--- .oh-my-zsh/custom/aliases.zsh
+++ .oh-my-zsh/custom/aliases.zsh
@@ -1,12 +1,16 @@
 #Dunno why I'm putting these here but w/e
 export VISUAL=vim
 export EDITOR=$VISUAL
+export GPG_TTY=$(tty)
 
 alias ls='ls --color=auto --group-directories-first'
 alias la='ls -A'
 alias lA='ls -Alh'
 alias ll='ls -lh'
 alias lb='lsblk -o name,size,fstype,mountpoint'
+
+alias mv='mv -i'
+alias cp='cp -i'
 
 alias grep='grep --color=auto'
 alias mkd='mkdir -pv'
@@ -28,6 +32,8 @@ alias ctemp='cat /sys/class/thermal/thermal_zone0/temp'
 alias please='sudo $(history -p !!)'
 alias pag='ps ax | grep -i'
 alias pav='pavucontrol'
+
+alias rec='ffmpeg -f alsa -ar 48000 -ac 1 -i'
 
 function acp() { git add . && git commit && git push }
 function acpp() { git add . && git commit && git pull && git push }
